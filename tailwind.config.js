70dd77360c51024db73e38a878347e06b7a83f39
OCKing <51097023+KDW1@users.noreply.github.com>
2022-08-16 18:09:08 -0500

    Essentially Done
    - Reminder: Need to add, resume display

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,9 @@ module.exports = {
     colors: {
       "display": "#49E17D",
       "secondary": "#3D7459",
-      "backdrop": "#1E2421"
+      "backdrop": "#1E2421",
+      "darkGrey": "#52634E", 
+      "darkTone": "#303D35"
     },
     extend: {},
   },
