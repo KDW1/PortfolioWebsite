676c1c820d96ef1b09f08de76819b13af6d3d858
OCKing <51097023+KDW1@users.noreply.github.com>
2022-08-17 11:19:03 -0500

    ReasponsiveDesign1
    ResponsiveDesign for Intro text

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}",
+  content: ["./docs/**/*.{html,js}",
   "./node_modules/flowbite/**/*.js"],
   theme: {
+    screens: {
+      '2xl': {'max':'1536px'},
+      xl: {'max':'1280px'},
+      lg: {'max':'1130px'},
+      md: {'max':'950px'},
+      sm: {'max':'735px'},
+      'xs': {'max': '550px'},
+      'bruh': {'max': '390px'}
+    },
     colors: {
       "display": "#49E17D",
       "secondary": "#3D7459",
